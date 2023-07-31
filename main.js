2f9e47f36f08b7b283861219e53f71f0c73bb3d0
Desisav10 <desislav.iliev12@abv.bg>
2023-07-31 12:38:31 +0300

    adding edit functionality

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ $(function(){
     //     };
     // });
    
-    $('.submit-btn').click(function(){
+    $('.add-btn').click(function(){
         let imput = $('.imp').val();
         if(imput!=''){
-    $('.to-do-list').append('<li class="mt-2">'  + imput + '<i class="bi bi-check-lg fa-2x check-btn m-2"></i>' +'<i class="bi bi-pencil-square edit-btn m-2"></i>'+'<i class="bi bi-trash remove-btn  lg m-2"></i> ');
-        };
+    $('.to-do-list').append('<li list-name class="mt-2 editable">'+ '<input type="text" class="edit" style="display:none"/>'+ imput + '<i class="bi bi-check-lg fa-2x check-btn m-2"></i>' +'<i class="bi bi-pencil-square edit-btn m-2"></i>'+'<i class="bi bi-trash remove-btn  lg m-2"></i> ');
+    $(".imp").val(null);
+    };
+   
     });
 
     $('ol').on('click', '.remove-btn',function(){
@@ -23,14 +25,37 @@ $(function(){
         $(this).parent('li').toggleClass('checked');
 
     });
+   
+    
+
+    $('ol').on('click', '.edit-btn',function(){
+     
+     $(this).parent('li').show().html('<input class="edit-input" type="text" value="'+ $(this).text() +'">'+ '<button class="btn btn-primary m-2 save-btn" type="button">Save</button>')
+         
+    });
+
+    $('ol').on('click', '.save-btn',function(){
+        let editInput = $('.edit-input').val();
+        if(editInput!=''){
+       
+        $(this).parent('li').html('<input type="text" class="edit" style="display:none"/>'+ editInput + '<i class="bi bi-check-lg fa-2x check-btn m-2"></i>' +'<i class="bi bi-pencil-square edit-btn m-2"></i>'+'<i class="bi bi-trash remove-btn  lg m-2"></i> ');
+        
+    };
+    
+        
+     });
 
 
     $('.submit-btn').click(function(){
 
     });
+
+
    
 
    
     
 
 });
+
+
