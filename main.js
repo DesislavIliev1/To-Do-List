dd6e0cfcc9152240bce28df3fe200693420b4b79
Desisav10 <desislav.iliev12@abv.bg>
2023-08-02 02:09:37 +0300

    drag and drop functionality

--- main.js
+++ main.js
@@ -6,11 +6,20 @@ $(function(){
     // ' <label class="form-check-label" for="flexCheckDefault">'+imput+ '<i class="bi bi-pencil-square m-2"></i>'+'<i class="bi bi-trash  lg m-2"></i> ');
     //     };
     // });
-   
+    
     $('.add-btn').click(function(){
-        let imput = $('.imp').val();
-        if(imput!=''){
-    $('.to-do-list').append('<li list-name class="mt-2 editable">'+ '<input type="text" class="edit" style="display:none"/>'+ imput + '<i class="bi bi-check-lg fa-2x check-btn m-2"></i>' +'<i class="bi bi-pencil-square edit-btn m-2"></i>'+'<i class="bi bi-trash remove-btn  lg m-2"></i> ');
+        let input = $('.imp').val();
+      //  let todosArray = [];
+      //  localStorage.setItem("todos", JSON.stringify(input));
+       
+      //  todosArray.push(input);
+       // todosArray = JSON.parse(localStorage.getItem("todos"))
+        
+       
+        
+       
+    if(input!=''){
+    $('.to-do-list').append('<li  list-name class="mt-2 position-relative ">'+ '<input type="text" class="edit" style="display:none"/>'+ input + '<i class="bi bi-check-lg fa-2x check-btn position-absolute top-0 end-0 me-5"></i>' +'<i class="bi bi-pencil-square edit-btn position-absolute top-0 end-0 me-4 "></i>'+'<i class="bi bi-trash remove-btn position-absolute top-0 end-0 me-1"></i></li>');
     $(".imp").val(null);
     };
    
@@ -30,7 +39,7 @@ $(function(){
 
     $('ol').on('click', '.edit-btn',function(){
      
-     $(this).parent('li').show().html('<input class="edit-input" type="text" value="'+ $(this).text() +'">'+ '<button class="btn btn-primary m-2 save-btn" type="button">Save</button>')
+     $(this).parent('li').show().html('<input class="edit-input" type="text" value="'+ $(this).text() +'">'+ '<button class="btn btn-success m-2 save-btn" type="button">Save</button>')
          
     });
 
@@ -38,24 +47,53 @@ $(function(){
         let editInput = $('.edit-input').val();
         if(editInput!=''){
        
-        $(this).parent('li').html('<input type="text" class="edit" style="display:none"/>'+ editInput + '<i class="bi bi-check-lg fa-2x check-btn m-2"></i>' +'<i class="bi bi-pencil-square edit-btn m-2"></i>'+'<i class="bi bi-trash remove-btn  lg m-2"></i> ');
+        $(this).parent('li').html('<input type="text" class="edit" style="display:none"/>'+ editInput + '<i class="bi bi-check-lg fa-2x check-btn position-absolute top-0 end-0 me-5"></i>' +'<i class="bi bi-pencil-square edit-btn position-absolute top-0 end-0 me-4 "></i>'+'<i class="bi bi-trash remove-btn position-absolute top-0 end-0 me-1 "></i></li>');
         
     };
     
         
      });
 
+     $('#sortable').sortable();({
 
-    $('.submit-btn').click(function(){
+     });
 
-    });
+       function saveLocalStorage(input){
+        let inputTask = localStorage.getItem("localTask");
+        if(inputTask == null){
+            taskObj = [];
+        }
+        else{
+            taskObj = JSON.parse(inputTask);
+
+        }
+        taskObj.push(input);
+        localStorage.setItem("localTask", JSON.stringify(taskObj));
+
+        
 
 
-   
+     }
 
-   
+
+
+
     
-
 });
+
+function submit(input) {
+    var names = document.getElementById("names").value;
+    var allNames = JSON.parse(localStorage.getItem("allNames")) || [];
+    allNames.push(input);
+    localStorage.setItem("allNames", JSON.stringify(allNames));
+    document.getElementById("names").value = '';
+    document.getElementById("namesList").innerHTML = localStorage.getItem("allNames");
+  }
+  
+  document.getElementById("namesList").innerHTML = localStorage.getItem("allNames");
+
+
+
+
 
 
